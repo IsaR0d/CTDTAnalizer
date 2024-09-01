9ed0c88a0961341f932253256a57e1d9efb618f2
Isauro Manuel Rodriguez <90867@sistemas.frc.utn.edu.ar>
2024-08-31 21:17:57 -0300

    Codigo compactado

--- controler.py
+++ controler.py
@@ -1,126 +1,118 @@
 import math
 
+def aplicar_modificaciones(diccionario_jugador, aumento_base, stats_a_modificar):
+    for stat in stats_a_modificar:
+        diccionario_jugador['stats'][stat] += aumento_base
+
 def modificar_por_lb(diccionario_jugador):
-    diccionario_jugador['stats']['potencia'] += 1000
-    diccionario_jugador['stats']['tecnica'] += 1000
-    diccionario_jugador['stats']['rapidez'] += 1000
+    stats_base = ['potencia', 'tecnica', 'rapidez']
+    aplicar_modificaciones(diccionario_jugador, 1000, stats_base)
+    
+    lb_config = diccionario_jugador['otros']['lb']
+    if lb_config == 'Todo (Atacante)':
+        stats_extra = ['regate', 'remate', 'pase', 'entrada', 'intercepcion']
+    elif lb_config == 'Todo (Defensor)':
+        stats_extra = ['regate', 'remate', 'pase', 'entrada', 'bloqueo']
+    elif lb_config == 'Ataque':
+        stats_extra = ['regate', 'remate', 'pase']
+    elif lb_config == 'Defensa':
+        stats_extra = ['entrada', 'bloqueo', 'intercepcion']
+    else:
+        stats_extra = []
 
-    if diccionario_jugador['otros']['lb'] == 'Todo (Atacante)':
-        diccionario_jugador['stats']['regate'] += 1000
-        diccionario_jugador['stats']['remate'] += 1000
-        diccionario_jugador['stats']['pase'] += 1000
-        diccionario_jugador['stats']['entrada'] += 1000
-        diccionario_jugador['stats']['intercepcion'] += 1000
-
-    elif diccionario_jugador['otros']['lb'] == 'Todo (Defensor)':
-        diccionario_jugador['stats']['regate'] += 1000
-        diccionario_jugador['stats']['remate'] += 1000
-        diccionario_jugador['stats']['pase'] += 1000
-        diccionario_jugador['stats']['entrada'] += 1000
-        diccionario_jugador['stats']['bloqueo'] += 1000
-
-    elif diccionario_jugador['otros']['lb'] == 'Ataque':
-        diccionario_jugador['stats']['regate'] += 1000
-        diccionario_jugador['stats']['remate'] += 1000
-        diccionario_jugador['stats']['pase'] += 1000
-
-    elif diccionario_jugador['otros']['lb'] == 'Defensa':
-        diccionario_jugador['stats']['entrada'] += 1000
-        diccionario_jugador['stats']['bloqueo'] += 1000
-        diccionario_jugador['stats']['intercepcion'] += 1000
+    aplicar_modificaciones(diccionario_jugador, 1000, stats_extra)
 
 def modificar_por_bb4(diccionario_jugador):
-    diccionario_jugador['stats']['potencia'] += 2400
-    diccionario_jugador['stats']['tecnica'] += 2400
-    diccionario_jugador['stats']['rapidez'] += 2400
-    diccionario_jugador['stats']['regate'] += 1200
-    diccionario_jugador['stats']['remate'] += 1200
-    diccionario_jugador['stats']['pase'] += 1200
-    diccionario_jugador['stats']['entrada'] += 1200
-    diccionario_jugador['stats']['bloqueo'] += 1200
-    diccionario_jugador['stats']['intercepcion'] += 1200
+    stats_base = ['potencia', 'tecnica', 'rapidez']
+    stats_extra = ['regate', 'remate', 'pase', 'entrada', 'bloqueo', 'intercepcion']
+    
+    aplicar_modificaciones(diccionario_jugador, 2400, stats_base)
+    aplicar_modificaciones(diccionario_jugador, 1200, stats_extra)
 
 def calcular_fuerza_tecnicas(diccionario_jugador):
     tecnicas_final = {}
-    #A todas las tecnicas, la multiplico por el aumento de potencia y por el extra que puedan tener
-    for i in diccionario_jugador['tecnicas']:
+    otros = diccionario_jugador['otros']
+    potencia_base = otros['potencia']
+    
+    for tecnica, valor in diccionario_jugador['tecnicas'].items():
         multiplicador_balones_aire = 0
-        if i == 'alto' or i == 'bajo':
-            if diccionario_jugador['otros']['cabeceo'] == 'Muy Bueno' or diccionario_jugador['otros']['volea'] == 'Muy Bueno':
-                multiplicador_balones_aire = 0.25
-            elif diccionario_jugador['otros']['cabeceo'] == 'Bueno' or diccionario_jugador['otros']['volea'] == 'Bueno':
-                multiplicador_balones_aire = 0.125
+        if tecnica in ['alto', 'bajo']:
+            if otros['cabeceo'] in ['Muy Bueno', 'Bueno'] or otros['volea'] in ['Muy Bueno', 'Bueno']:
+                multiplicador_balones_aire = 0.25 if otros['cabeceo'] == 'Muy Bueno' or otros['volea'] == 'Muy Bueno' else 0.125
             
-        tecnicas_final[i] = ((diccionario_jugador['tecnicas'][i] + multiplicador_balones_aire) *((diccionario_jugador['otros']['potencia'])+diccionario_jugador['extras'][i]-1))
-        print(diccionario_jugador['tecnicas'][i], multiplicador_balones_aire, diccionario_jugador['otros']['potencia'], diccionario_jugador['extras'][i], tecnicas_final[i])
-    if diccionario_jugador['otros']['volea'] == 'Muy Bueno':
-        tecnicas_final['bloqueoBajo'] = (diccionario_jugador['tecnicas']['bloqueo']+0.25)*((diccionario_jugador['otros']['potencia'])+diccionario_jugador['extras']['bloqueo']-1)
-    elif diccionario_jugador['otros']['volea'] == 'Bueno':
-        tecnicas_final['bloqueoBajo'] = (diccionario_jugador['tecnicas']['bloqueo']+0.125)*((diccionario_jugador['otros']['potencia'])+diccionario_jugador['extras']['bloqueo']-1)
-    else:
-        tecnicas_final['bloqueoBajo'] = tecnicas_final['bloqueo']
-    if diccionario_jugador['otros']['cabeceo'] == 'Muy Bueno':
-        tecnicas_final['bloqueoAlto'] = (diccionario_jugador['tecnicas']['bloqueo']+0.25)*((diccionario_jugador['otros']['potencia'])+diccionario_jugador['extras']['bloqueo']-1)
-    elif diccionario_jugador['otros']['cabeceo'] == 'Bueno':
-        tecnicas_final['bloqueoAlto'] = (diccionario_jugador['tecnicas']['bloqueo']+0.125)*((diccionario_jugador['otros']['potencia'])+diccionario_jugador['extras']['bloqueo']-1)
-    else:
-        tecnicas_final['bloqueoAlto'] = tecnicas_final['bloqueo']
+        tecnicas_final[tecnica] = ((valor + multiplicador_balones_aire) *
+                                   (potencia_base + diccionario_jugador['extras'][tecnica] - 1))
+
+    tecnicas_final['bloqueoBajo'] = tecnicas_final['bloqueo']
+    tecnicas_final['bloqueoAlto'] = tecnicas_final['bloqueo']
+
+    if otros['volea'] == 'Muy Bueno':
+        tecnicas_final['bloqueoBajo'] = (diccionario_jugador['tecnicas']['bloqueo'] + 0.25) * (potencia_base + diccionario_jugador['extras']['bloqueo'] - 1)
+    elif otros['volea'] == 'Bueno':
+        tecnicas_final['bloqueoBajo'] = (diccionario_jugador['tecnicas']['bloqueo'] + 0.125) * (potencia_base + diccionario_jugador['extras']['bloqueo'] - 1)
+
+    if otros['cabeceo'] == 'Muy Bueno':
+        tecnicas_final['bloqueoAlto'] = (diccionario_jugador['tecnicas']['bloqueo'] + 0.25) * (potencia_base + diccionario_jugador['extras']['bloqueo'] - 1)
+    elif otros['cabeceo'] == 'Bueno':
+        tecnicas_final['bloqueoAlto'] = (diccionario_jugador['tecnicas']['bloqueo'] + 0.125) * (potencia_base + diccionario_jugador['extras']['bloqueo'] - 1)
+
     return tecnicas_final
 
 def calcular_stats_visuales(diccionario_jugador, multiplicador_equipo, multiplicador_color):
-    stat_final_regate = math.ceil(diccionario_jugador['stats']['regate'] * multiplicador_color * diccionario_jugador['exStats']['regate'] * multiplicador_equipo) + ((diccionario_jugador['stats']['rapidez']* multiplicador_color * multiplicador_equipo)/2)
-    stat_final_remate = math.ceil(diccionario_jugador['stats']['remate']* multiplicador_color * diccionario_jugador['exStats']['remate'] * multiplicador_equipo) + ((diccionario_jugador['stats']['potencia']* multiplicador_color * multiplicador_equipo)/2)
-    stat_final_pase = math.ceil(diccionario_jugador['stats']['pase']* multiplicador_color * diccionario_jugador['exStats']['pase'] * multiplicador_equipo) + ((diccionario_jugador['stats']['tecnica']* multiplicador_color * multiplicador_equipo)/2)
-    stat_final_entrada = math.ceil(diccionario_jugador['stats']['entrada']* multiplicador_color * diccionario_jugador['exStats']['entrada'] * multiplicador_equipo) + ((diccionario_jugador['stats']['rapidez']* multiplicador_color * multiplicador_equipo)/2)
-    stat_final_bloqueo = math.ceil(diccionario_jugador['stats']['bloqueo']* multiplicador_color * diccionario_jugador['exStats']['bloqueo'] * multiplicador_equipo) + ((diccionario_jugador['stats']['potencia']* multiplicador_color * multiplicador_equipo)/2)
-    stat_final_intercepcion = math.ceil(diccionario_jugador['stats']['intercepcion']* multiplicador_color * diccionario_jugador['exStats']['intercepcion'] * multiplicador_equipo) + ((diccionario_jugador['stats']['tecnica']* multiplicador_color * multiplicador_equipo)/2)
-    stats_final = {'regate': stat_final_regate, 'remate': stat_final_remate, 'pase': stat_final_pase, 'entrada': stat_final_entrada, 'bloqueo': stat_final_bloqueo, 'intercepcion': stat_final_intercepcion}
-    return stats_final
+    stats = diccionario_jugador['stats']
+    exStats = diccionario_jugador['exStats']
+    
+    def calcular_stat(stat, extra_stat):
+        return math.ceil(stats[stat] * multiplicador_color * exStats[stat] * multiplicador_equipo + 
+                         (stats[extra_stat] * multiplicador_color * multiplicador_equipo) / 2)
+
+    return {
+        'regate': calcular_stat('regate', 'rapidez'),
+        'remate': calcular_stat('remate', 'potencia'),
+        'pase': calcular_stat('pase', 'tecnica'),
+        'entrada': calcular_stat('entrada', 'rapidez'),
+        'bloqueo': calcular_stat('bloqueo', 'potencia'),
+        'intercepcion': calcular_stat('intercepcion', 'tecnica')
+    }
 
 def calcular_stat_duelo(diccionario_jugador, multiplicador_equipo, tecnicas_final):
-    multiplicador_color = 1
-    if diccionario_jugador['otros']['color'] == True:
-        multiplicador_color = 1.25
-    stats_sin_tecnica = calcular_stats_visuales(diccionario_jugador, multiplicador_equipo , multiplicador_color)
-    print(stats_sin_tecnica)
-    print(tecnicas_final)
-    reg_stat_duelo = math.ceil(stats_sin_tecnica['regate']*(tecnicas_final['regate']/100))
-    rem_stat_duelo = math.ceil(stats_sin_tecnica['remate']*(tecnicas_final['remate']/100))
-    pase_stat_duelo = math.ceil(stats_sin_tecnica['pase']*(tecnicas_final['pase']/100))
-    ent_stat_duelo = math.ceil(stats_sin_tecnica['entrada']*(tecnicas_final['entrada']/100))
-    blo_stat_duelo = math.ceil(stats_sin_tecnica['bloqueo']*(tecnicas_final['bloqueo']/100))
-    int_stat_duelo = math.ceil(stats_sin_tecnica['intercepcion']*(tecnicas_final['intercepcion']/100))
-    cab_stat_duelo = math.ceil(stats_sin_tecnica['remate']*(tecnicas_final['alto']/100))
-    vol_stat_duelo = math.ceil(stats_sin_tecnica['remate']*(tecnicas_final['bajo']/100))
-    balto_stat_duelo = math.ceil(stats_sin_tecnica['bloqueo']*(tecnicas_final['bloqueoAlto']/100))
-    bbajo_stat_duelo = math.ceil(stats_sin_tecnica['bloqueo']*(tecnicas_final['bloqueoBajo']/100))
-    print(reg_stat_duelo, rem_stat_duelo, pase_stat_duelo, ent_stat_duelo, blo_stat_duelo, int_stat_duelo, cab_stat_duelo, vol_stat_duelo, balto_stat_duelo, bbajo_stat_duelo)
-    stats_duelo = {'regate': reg_stat_duelo, 'remate': rem_stat_duelo, 'pase': pase_stat_duelo, 'entrada': ent_stat_duelo, 'bloqueo': blo_stat_duelo, 'intercepcion': int_stat_duelo, 'cabeceo': cab_stat_duelo, 'volea': vol_stat_duelo, 'bloqueoAlto': balto_stat_duelo, 'bloqueoBajo': bbajo_stat_duelo}
-    return stats_duelo
+    multiplicador_color = 1.25 if diccionario_jugador['otros']['color'] else 1
+    stats_sin_tecnica = calcular_stats_visuales(diccionario_jugador, multiplicador_equipo, multiplicador_color)
+    
+    def calcular_duelo(stat, tecnica):
+        return math.ceil(stats_sin_tecnica[stat] * (tecnicas_final[tecnica] / 100))
 
+    return {
+        'regate': calcular_duelo('regate', 'regate'),
+        'remate': calcular_duelo('remate', 'remate'),
+        'pase': calcular_duelo('pase', 'pase'),
+        'entrada': calcular_duelo('entrada', 'entrada'),
+        'bloqueo': calcular_duelo('bloqueo', 'bloqueo'),
+        'intercepcion': calcular_duelo('intercepcion', 'intercepcion'),
+        'cabeceo': calcular_duelo('remate', 'alto'),
+        'volea': calcular_duelo('remate', 'bajo'),
+        'bloqueoAlto': calcular_duelo('bloqueo', 'bloqueoAlto'),
+        'bloqueoBajo': calcular_duelo('bloqueo', 'bloqueoBajo')
+    }
 
 def analizar_jugador(progress, diccionario_jugador):
-
-    #---------------------------trabajo sobre las stats base----------------------------------------------------------------------
-    #sumo los parametros del bb4
-    if diccionario_jugador['otros']['bb4'] == True:
+    # Modificación por BB4
+    if diccionario_jugador['otros'].get('bb4', False):
         modificar_por_bb4(diccionario_jugador)
     
-    #sumo los parametros del lb
-    if diccionario_jugador['otros']['lb'] == 'Nada':
-        pass
-    else:
+    # Modificación por LB
+    if diccionario_jugador['otros']['lb'] != 'Nada':
         modificar_por_lb(diccionario_jugador)
-    #---------------------------------trabajo sobre las tecnicas-------------------------------------------------------------
+    
+    # Cálculo de fuerza de técnicas
     tecnicas_final = calcular_fuerza_tecnicas(diccionario_jugador)
     
-    #---------------------------------trabajo con ts y statsup---------------------------------------------------------------
-    multiplicador_equipo = diccionario_jugador['otros']['ts']*(diccionario_jugador['otros']['bond']+diccionario_jugador['otros']['parametros']-1)
-
+    # Cálculo de multiplicador del equipo
+    otros = diccionario_jugador['otros']
+    multiplicador_equipo = otros['ts'] * (otros['bond'] + otros['parametros'] - 1)
+    
+    # Cálculo de estadísticas visuales y de duelo
     stats_visual_final = calcular_stats_visuales(diccionario_jugador, multiplicador_equipo, 1)
-    #---------------------------------trabajo con las tecnicas---------------------------------------------------------------    
     stats_duelo_final = calcular_stat_duelo(diccionario_jugador, multiplicador_equipo, tecnicas_final)
-
-
-    diccionario_mostrar = {'statsVisuales': stats_visual_final, 'statsDuelo': stats_duelo_final}
-    return diccionario_mostrar
+    
+    return {'statsVisuales': stats_visual_final, 'statsDuelo': stats_duelo_final}
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import *
-from PyQt6.uic import *
+from PyQt6.uic import loadUi
 from PyQt6.QtCore import QThread, pyqtSignal
 from controler import analizar_jugador
 import math
@@ -8,57 +8,96 @@ import math
 class HiloAnalisis(QThread):
     result = pyqtSignal(dict)
     progress = pyqtSignal(int)
+
     def __init__(self, diccionario_jugador):
         super().__init__()
         self.diccionario_jugador = diccionario_jugador
-    
-    #comienza a correr el hilo
+
     def run(self):
         result = analizar_jugador(self.progress, self.diccionario_jugador)
         self.result.emit(result)
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
         loadUi('analisis.ui', self)
         self.show()
         self.resultado_analisis = None
         self.calcular_btn.clicked.connect(self.calcular)
 
     def onTaskFinished(self, result):
-        self.rem_visual.setText(str(result['statsVisuales']['remate']))
-        self.pas_visual.setText(str(result['statsVisuales']['pase']))
-        self.par_visual.setText(str(result['statsVisuales']['pase']))
-        self.reg_visual.setText(str(result['statsVisuales']['regate']))
-        self.ent_visual.setText(str(result['statsVisuales']['entrada']))
-        self.blo_visual.setText(str(result['statsVisuales']['bloqueo']))
-        self.int_visual.setText(str(result['statsVisuales']['intercepcion']))
+        for key in ['remate', 'pase', 'regate', 'entrada', 'bloqueo', 'intercepcion']:
+            getattr(self, f"{key[:3]}_visual").setText(str(math.ceil(result['statsVisuales'][key])))
+            getattr(self, f"{key[:3]}_total").setText(str(math.ceil(result['statsDuelo'][key])))
 
-
-        print(result['statsDuelo'])
-        self.rem_total.setText(str(result['statsDuelo']['remate']))
-        self.pas_total.setText(str(result['statsDuelo']['pase']))
-        #self.par_total.setText(str(result['statsDuelo']['pared']))
-        self.reg_total.setText(str(result['statsDuelo']['regate']))
-        self.ent_total.setText(str(result['statsDuelo']['entrada']))
-        self.blo_total.setText(str(result['statsDuelo']['bloqueo']))
-        self.int_total.setText(str(result['statsDuelo']['intercepcion']))
-        self.cab_total.setText(str(result['statsDuelo']['cabeceo']))
-        self.vol_total.setText(str(result['statsDuelo']['volea']))
-        self.balto_total.setText(str(result['statsDuelo']['bloqueoAlto']))
-        self.bbajo_total.setText(str(result['statsDuelo']['bloqueoBajo']))
-
-
+        self.cab_total.setText(str(math.ceil(result['statsDuelo']['cabeceo'])))
+        self.vol_total.setText(str(math.ceil(result['statsDuelo']['volea'])))
+        self.balto_total.setText(str(math.ceil(result['statsDuelo']['bloqueoAlto'])))
+        self.bbajo_total.setText(str(math.ceil(result['statsDuelo']['bloqueoBajo'])))
 
     def onProgress(self, progress):
         self.progressBar.setValue(progress)
 
     def calcular(self):   
-        stats ={'regate': self.reg_st.value(), 'remate': self.rem_st.value(), 'pase': self.pas_st.value(), 'entrada': self.ent_st.value(), 'bloqueo': self.blo_st.value(), 'intercepcion': self.int_st.value(), 'rapidez': self.rap_st.value(), 'potencia': self.pot_st.value(), 'tecnica': self.tec_st.value()}
-        tecnicas = {'regate': self.reg_tc.value(), 'remate': self.rem_tc.value(), 'pase': self.pas_tc.value(), 'entrada': self.ent_tc.value(), 'bloqueo': self.blo_tc.value(), 'intercepcion': self.int_tc.value(), 'bajo': self.baj_tc.value(), 'alto': self.alt_tc.value()}
-        extras = {'regate': 1+(self.reg_ex.value()+self.ex_tec.value())/100, 'remate': 1+(self.rem_ex.value()+self.ex_tec.value())/100, 'pase': 1+(self.pas_ex.value()+self.ex_tec.value())/100, 'entrada': 1+(self.ent_ex.value()+self.ex_tec.value())/100, 'bloqueo': 1+(self.blo_ex.value()+self.ex_tec.value())/100, 'intercepcion': 1+(self.int_ex.value()+self.ex_tec.value())/100, 'bajo': 1+(self.baj_ex.value()+self.ex_tec.value())/100, 'alto': 1+(self.alt_ex.value()+self.ex_tec.value())/100}
-        extra_stat = {'regate': 1+(self.reg_ex_stat.value()+self.ex_stat.value())/100, 'remate': 1+(self.rem_ex_stat.value()+self.ex_stat.value())/100, 'pase': 1+(self.pas_ex_stat.value()+self.ex_stat.value())/100, 'entrada': 1+(self.ent_ex_stat.value()+self.ex_stat.value())/100, 'bloqueo': 1+(self.blo_ex_stat.value()+self.ex_stat.value())/100, 'intercepcion': 1+(self.int_ex_stat.value()+self.ex_stat.value())/100, 'rapidez': 1+(self.rap_ex_stat.value()+self.ex_stat.value())/100, 'potencia': 1+(self.pot_ex_stat.value()+self.ex_stat.value())/100, 'tecnica': 1+(self.tec_ex_stat.value()+self.ex_stat.value())/100}
-        otros = {'ts': 1+self.ts.value()/100, 'bond': 1+self.bond.value()/100, 'parametros': 1+self.parametros.value()/100, 'potencia': 1+self.potencia.value()/100, 'cabeceo': self.cab_sel.currentText(), 'volea': self.vol_sel.currentText(), 'formacion': self.for_sel.currentText(), 'lb': self.lb_sel.currentText(), 'color': self.col_sel.isChecked(), 'bb4': self.bb4_cb.isChecked()}
+        stats = {
+            'regate': self.reg_st.value(), 
+            'remate': self.rem_st.value(), 
+            'pase': self.pas_st.value(), 
+            'entrada': self.ent_st.value(), 
+            'bloqueo': self.blo_st.value(), 
+            'intercepcion': self.int_st.value(), 
+            'rapidez': self.rap_st.value(), 
+            'potencia': self.pot_st.value(), 
+            'tecnica': self.tec_st.value()
+        }
+
+        tecnicas = {
+            'regate': self.reg_tc.value(), 
+            'remate': self.rem_tc.value(), 
+            'pase': self.pas_tc.value(), 
+            'entrada': self.ent_tc.value(), 
+            'bloqueo': self.blo_tc.value(), 
+            'intercepcion': self.int_tc.value(), 
+            'bajo': self.baj_tc.value(), 
+            'alto': self.alt_tc.value()
+        }
+
+        extras = {
+            'regate': 1 + (self.reg_ex.value() + self.ex_tec.value()) / 100, 
+            'remate': 1 + (self.rem_ex.value() + self.ex_tec.value()) / 100, 
+            'pase': 1 + (self.pas_ex.value() + self.ex_tec.value()) / 100, 
+            'entrada': 1 + (self.ent_ex.value() + self.ex_tec.value()) / 100, 
+            'bloqueo': 1 + (self.blo_ex.value() + self.ex_tec.value()) / 100, 
+            'intercepcion': 1 + (self.int_ex.value() + self.ex_tec.value()) / 100, 
+            'bajo': 1 + (self.baj_ex.value() + self.ex_tec.value()) / 100, 
+            'alto': 1 + (self.alt_ex.value() + self.ex_tec.value()) / 100
+        }
+
+        extra_stat = {
+            'regate': 1 + (self.reg_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'remate': 1 + (self.rem_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'pase': 1 + (self.pas_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'entrada': 1 + (self.ent_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'bloqueo': 1 + (self.blo_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'intercepcion': 1 + (self.int_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'rapidez': 1 + (self.rap_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'potencia': 1 + (self.pot_ex_stat.value() + self.ex_stat.value()) / 100, 
+            'tecnica': 1 + (self.tec_ex_stat.value() + self.ex_stat.value()) / 100
+        }
+
+        otros = {
+            'ts': 1 + self.ts.value() / 100, 
+            'bond': 1 + self.bond.value() / 100, 
+            'parametros': 1 + self.parametros.value() / 100, 
+            'potencia': 1 + self.potencia.value() / 100, 
+            'cabeceo': self.cab_sel.currentText(), 
+            'volea': self.vol_sel.currentText(), 
+            'formacion': self.for_sel.currentText(), 
+            'lb': self.lb_sel.currentText(), 
+            'color': self.col_sel.isChecked(), 
+            'bb4': self.bb4_cb.isChecked()
+        }
+
         diccionario_jugador = {'stats': stats, 'tecnicas': tecnicas, 'extras': extras, 'otros': otros, 'exStats': extra_stat}
 
         # Crear el hilo y pasar el diccionario como argumento
@@ -71,4 +110,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
     window = MainWindow()
-    sys.exit(app.exec())    
+    sys.exit(app.exec())
